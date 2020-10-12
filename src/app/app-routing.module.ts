a79461e219d4c4fa15558f52f1d5b67e3e2b5ef3
Yevhen Chernenko <chernenko.eu@gmail.com>
2020-10-12 12:22:35 +0300

    Setting up router, home module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,20 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+  },
+  // {
+  //   path: 'season',
+  //   loadChildren: () => import('./about/about.module').then(m => m.AboutModule)
+  // },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
