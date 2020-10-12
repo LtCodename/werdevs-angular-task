7e855d9399d184577cc75845536cb620370bad26
Yevhen Chernenko <chernenko.eu@gmail.com>
2020-10-12 12:57:59 +0300

    header css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'werdevs-angular-task';
+  currentRoute: string;
+
+  constructor(private router: Router) {
+    router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        switch(this.router.url) {
+          case "/directors":
+            this.currentRoute = 'home';
+          break;
+          case "/about":
+            this.currentRoute = 'about';
+          break;
+            default:
+              this.currentRoute = 'home';
+        }
+      }
+    });
+  }
 }
